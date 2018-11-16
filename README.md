d38ebfa07c5a5560be678e79bf2f225a14e6c4e0
aleclarson <alec.stanford.larson@gmail.com>
2018-11-16 09:26:57 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# vscode-packagephobia
+
+Fork of [import-cost](https://github.com/wix/import-cost) that uses [PackagePhobia](https://packagephobia.now.sh) instead of calculating sizes itself.
---
+++ src/extension.ts
@@ -0,0 +1,50 @@
+import {importCost, cleanup, JAVASCRIPT, TYPESCRIPT} from 'import-cost';
+import {ExtensionContext, window, workspace} from 'vscode';
+import {calculated, flushDecorations} from './decorator';
+import logger from './logger';
+
+export function activate(context: ExtensionContext) {
+  try {
+    logger.init(context);
+    logger.log('starting...');
+    workspace.onDidChangeTextDocument(ev => processActiveFile(ev.document));
+    window.onDidChangeActiveTextEditor(ev => ev && processActiveFile(ev.document));
+    if (window.activeTextEditor) {
+      processActiveFile(window.activeTextEditor.document);
+    }
+  } catch (e) {
+    logger.log('wrapping error: ' + e);
+  }
+}
+
+export function deactivate() {
+  cleanup();
+}
+
+let emitters = {};
+async function processActiveFile(document) {
+  if (document && language(document)) {
+    const {fileName} = document;
+    if (emitters[fileName]) {
+      emitters[fileName].removeAllListeners();
+    }
+    emitters[fileName] = importCost(fileName, document.getText(), language(document));
+    emitters[fileName].on('error', e => logger.log(`importCost error: ${e}`));
+    emitters[fileName].on('start', packages => flushDecorations(fileName, packages));
+    emitters[fileName].on('calculated', packageInfo => calculated(packageInfo));
+    emitters[fileName].on('done', packages => flushDecorations(fileName, packages));
+  }
+}
+
+function language({fileName, languageId}) {
+  const configuration = workspace.getConfiguration('importCost');
+  const typescriptRegex = new RegExp(configuration.typescriptExtensions.join('|'));
+  const javascriptRegex = new RegExp(configuration.javascriptExtensions.join('|'));
+  if (languageId === 'typescript' || languageId === 'typescriptreact' || typescriptRegex.test(fileName)) {
+    return TYPESCRIPT;
+  } else if (languageId === 'javascript' || languageId === 'javascriptreact' || javascriptRegex.test(fileName)) {
+    return JAVASCRIPT;
+  } else {
+    return undefined;
+  }
+}
